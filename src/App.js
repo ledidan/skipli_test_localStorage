8e9e7d932976f3e190f02c9917baa2c97175b448
Bon <didan.mobe@gmail.com>
2023-11-22 11:47:15 +0700

    Add code

--- src/App.js
+++ src/App.js
@@ -1,25 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { v4 as uuidv4 } from 'uuid';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import logo from "./logo.svg";
+import "./App.css";
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    // Initialize state with an empty UUID
+    this.state = {
+      uuid: "",
+    };
+  }
+
+  componentDidMount() {
+    // Check if UUID is already in local storage
+    const storedUUID = localStorage.getItem("uuid");
+
+    if (storedUUID) {
+      // If UUID exists, use it
+      this.setState({ uuid: storedUUID });
+    } else {
+      // If UUID doesn't exist, generate a new one and save it to local storage
+      const newUUID = uuidv4();
+      localStorage.setItem("uuid", newUUID);
+      this.setState({ uuid: newUUID });
+    }
+  }
+
+  render() {
+    const { uuid } = this.state;
+
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1>ROOT DOMAIN SKIPLI LOCAL STORAGE</h1>
+          <p>UUID: {uuid}</p>
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
